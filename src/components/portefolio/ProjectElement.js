5a98b64c12e694eb5915a2b22014b1fd224b8d9b
unknown <rochefeuille.ophelie@gmail.com>
2021-07-12 13:32:50 +0200

    divstyle portefolio list

--- src/components/portefolio/ProjectElement.js
+++ src/components/portefolio/ProjectElement.js
@@ -7,11 +7,16 @@ import './projectElement.scss';
 export default class ProjectElement extends Component {
 
     render() {
+        const divStyleProjects = {
+            backgroundImage: 'url(' + this.props.project.img +')',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'};
+
         return (
          
             <div className='main-container-portefolio-project'>
                 <div className='container-project-portefolio'>
-                <img className="img-porteflio" alt="project" src={this.props.project.img} />
+                <div className="img-portefolio"  style={divStyleProjects} />
                 <div className='container-project-details-porteflio'>
                     <h1 className='title-project-portefoliolist'>{ this.props.project.title}</h1>
                     <span className='name-company'>{ this.props.project.company}</span>
